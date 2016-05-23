5386054a5e132229bf47d16bcfacfaccc4454d9d
kimber <kimber.gee@gmail.com>
2016-05-23 19:47:08 -0400

    assignment 45 complete but need to fix user favorites view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,22 @@
 module UsersHelper
+
+  def has_posts?(user)
+    if @user.posts.count > 0
+      return true
+    end
+  end
+
+  def has_comments?(user)
+    if @user.comments.count > 0
+      return true
+    end
+  end
+
+  def has_favorites?(user)
+    if @user.favorites.count > 0
+      return true
+      end
+    end
+
+
 end
