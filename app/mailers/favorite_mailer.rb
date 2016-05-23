800e24f0322b8ce8f9dbed0a288410839c1c75a3
kimber <kimber.gee@gmail.com>
2016-05-23 08:22:48 -0400

    assignment 44 complete

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,4 +12,15 @@ class FavoriteMailer < ApplicationMailer
 
     mail(to: user.email, subject: "New comment on #{post.title}")
   end
+
+  def new_post(post)
+    headers["Message-ID"] = "<post/#{post.id}@gentle-forest-26737.herokuapp.com>"
+    headers["In-Reply-To"] = "<post/#{post.id}@gentle-forest-26737.herokuapp.com>"
+    headers["References"] = "<post/#{post.id}@gentle-forest-26737.herokuapp.com>"
+
+    @post = post
+
+    mail(to: post.user.email, subject: "You will now receive all activity on #{post.title}")
+  end
+
 end
